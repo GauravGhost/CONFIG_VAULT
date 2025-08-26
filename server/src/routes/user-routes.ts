b13386afc7a174e8ee75f6f2f8acd0b11ddfbc28
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-08-26 11:31:47 +0530

    Added custom router

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,13 +1,11 @@
-import { Router } from 'express';
+import router from '../lib/async-router.js'
 import UserController from '../controllers/user-controller.js';
 
-const router = Router();
 const userController = new UserController();
 
-// User routes
-router.post('/', userController.createUser);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.postAsync('/', userController.createUser);
+router.getAsync('/:id', userController.getUserById);
+router.putAsync('/:id', userController.updateUser);
+router.deleteAsync('/:id', userController.deleteUser);
 
 export default router;
