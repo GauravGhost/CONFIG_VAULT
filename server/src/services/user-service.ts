1be5f87d9f02823ac0f18cfd3d221cd7169f3ae3
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-08-26 11:34:14 +0530

    Added encryption for password

--- server/src/services/user-service.ts
+++ server/src/services/user-service.ts
@@ -1,18 +1,38 @@
+import bcrypt from 'bcrypt';
+
 import UserRepository from "../repository/user-repository.js";
+import ApiError from "../utils/error.js";
+import type { User } from '@config-vault/shared';
 
 class UserService {
     private readonly userRepository: UserRepository;
-    
-    constructor(){
+
+    constructor() {
         this.userRepository = new UserRepository();
     }
 
-    public async createUser(data: any): Promise<any> {
+    public async getUserByUsername(username: string): Promise<any> {
+        const user = await this.userRepository.findOne({ username });
+        return user;
+    }
+
+    public async createUser(data: User): Promise<any> {
+        data.password = await this.encryptPassword(data.password);
         return this.userRepository.create(data);
     }
 
+    private async encryptPassword(password: string) {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    }
+
     public async getUserById(id: string): Promise<any> {
-        return this.userRepository.findById(id);
+        const user = await this.userRepository.findById(id);
+        if (!user) {
+            throw new ApiError('User not found', 404);
+        }
+        return user;
     }
 
     public async updateUser(id: string, data: any): Promise<any> {
