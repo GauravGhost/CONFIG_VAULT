07a984b29e1ab29640a397e07314eb3d953908d3
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-08-31 12:46:37 +0530

    Fixed breadcrumb height issue

--- client/src/components/core/MyBreadcrumb.tsx
+++ client/src/components/core/MyBreadcrumb.tsx
@@ -23,27 +23,27 @@ type MyBreadcrumbProps = {
 }
 function MyBreadcrumb({ items, actionComponent }: Readonly<MyBreadcrumbProps>) {
     return (
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2 py-2 px-4">
-            <Breadcrumb>
+        <div className="flex items-center justify-between py-2 px-4 min-h-[50px] gap-2">
+            <Breadcrumb className="flex-1 min-w-0">
                 <BreadcrumbList className="flex items-center flex-wrap">
                     {items.map((item, index) => (
                         <React.Fragment key={item.id}>
                             <BreadcrumbItem>
                                 <BreadcrumbLink asChild>
-                                    <Link to={item?.url ?? "#"} className="flex items-center gap-1">
-                                        {item.icon && <Icon name={item.icon} className="h-4 w-4" />} 
-                                        {item.label}
+                                    <Link to={item?.url ?? "#"} className="flex items-center gap-1 text-sm">
+                                        {item.icon && <Icon name={item.icon} className="h-3 w-3 sm:h-4 sm:w-4" />} 
+                                        <span className="truncate">{item.label}</span>
                                     </Link>
                                 </BreadcrumbLink>
                             </BreadcrumbItem>
-                            {index < items.length - 1 && <BreadcrumbSeparator />}
+                            {index < items.length - 1 && <BreadcrumbSeparator className="mx-1" />}
                         </React.Fragment>
                     ))}
                 </BreadcrumbList>
             </Breadcrumb>
 
             {actionComponent && (
-                <div className="flex items-center">
+                <div className="flex items-center flex-shrink-0 ml-2">
                     {actionComponent}
                 </div>
             )}
