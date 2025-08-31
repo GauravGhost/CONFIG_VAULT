eda0eaf6c2f2efe97541db356d1643a211a94987
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-08-31 14:29:31 +0530

    update: migration file

--- server/src/lib/migrations.ts
+++ server/src/lib/migrations.ts
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS configuration_detail (
     id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || substr(lower(hex(randomblob(2))),2) || '-' || substr('89ab',abs(random()) % 4 + 1, 1) || substr(lower(hex(randomblob(2))),2) || '-' || lower(hex(randomblob(6)))),
     configuration_id TEXT NOT NULL REFERENCES configurations(id) ON DELETE CASCADE,
     environment TEXT NOT NULL,
-    env TEXT NOT NULL,
+    env TEXT NULL,
     code TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
