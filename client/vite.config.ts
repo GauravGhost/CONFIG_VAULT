3e92344fed52ab5929475be4cea8bab3143f8589
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-09-02 23:09:20 +0530

    add config on frontend to support docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,15 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
